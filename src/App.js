bcb8f3cd61f96945e7d3736d9e1f4db48a6d4d31
ShuvenduSamanta <shuvendu247@gmail.com>
2022-06-17 22:06:21 +0530

    add individual components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+import Nav from  './components/nav/Nav'
+import About from './components/about/About';
+import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Header />
+      <Nav />
+      <About />
+      <Portfolio />
+      <Contact /> 
+      <Footer/>
+
+
+      {/* <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +30,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }

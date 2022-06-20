58187c3c175017c9d7c5612c0646f744a8df5441
ShuvenduSamanta <shuvendu247@gmail.com>
2022-06-20 13:09:54 +0530

    assets & components

--- src/App.js
+++ src/App.js
@@ -16,21 +16,6 @@ function App() {
       <Portfolio />
       <Contact /> 
       <Footer/>
-
-
-      {/* <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Shuvendu Samanta</h1>
+        <h5 className='text-light'>Computer Science Expert</h5>
+        <CTA />
+      </div>
+    </header>
   )
 }
 
